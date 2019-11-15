556c08cfd97b3560251c3606e471fbf357248f9c
Brayan Hernandez <39144645+Brayan1013@users.noreply.github.com>
2019-11-15 16:37:06 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDkFqVcCCPoe1AlkGJ1jfMjN2rScth8BrA",
-    authDomain: "clima2-b109d.firebaseapp.com",
-    databaseURL: "https://clima2-b109d.firebaseio.com",
-    projectId: "clima2-b109d",
-    storageBucket: "clima2-b109d.appspot.com",
-    messagingSenderId: "857095604486",
-    appId: "1:857095604486:web:570fb44209d82e80"
+    apiKey: "<key>",
+    authDomain: "<key>",
+    databaseURL: "<key>",
+    projectId: "<key>",
+    storageBucket: "<key>",
+    messagingSenderId: "<key>",
+    appId: "<key>"
 
   },
 
-  googleMapsKey: 'AIzaSyCWDHZKx51ybHQZBGLon3Nwas7xAHmeYR8'
+  googleMapsKey: '<key>'
 };
